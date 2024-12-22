980d4112a17c06306ab1325456d937abdeb56fb9
OlehYa <olehyakuts@gmail.com>
2024-12-22 12:23:19 +0100

    Creating a Tcp Listener

--- ChatCerver/Program.cs
+++ ChatCerver/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace ChatCerver
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ChatCerver
 {
     internal class Program
     {
+        static TcpListener _listener;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
+            _listener.Start();
+
+            var client = _listener.AcceptTcpClient();
+            Console.WriteLine("Client has connected");
         }
     }
 }
