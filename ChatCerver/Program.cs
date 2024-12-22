2c321996c2b1cb189e85c219d42cf4a97756c979
OlehYa <olehyakuts@gmail.com>
2024-12-22 12:38:24 +0100

    add new client

--- ChatCerver/Program.cs
+++ ChatCerver/Program.cs
@@ -5,14 +5,19 @@ namespace ChatCerver
 {
     internal class Program
     {
+        static List<Client> _users;
         static TcpListener _listener;
         static void Main(string[] args)
         {
+            _users = new List<Client>();
             _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
             _listener.Start();
 
-            var client = _listener.AcceptTcpClient();
-            Console.WriteLine("Client has connected");
+            while (true)
+            {
+                var client = new Client(_listener.AcceptTcpClient());
+                _users.Add(client);
+            }
         }
     }
 }
